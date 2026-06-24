5f69de5467db2222d41993764695c522d61a23b6
Adinda Rahmalia Jelita <adindarahmalia10@gmail.com>
2026-06-24 12:34:54 +0700

    feat: initialize controller dependencies

--- src/controller/KnowledgeController.java
+++ src/controller/KnowledgeController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import model.KnowledgeRepository;
+import util.InputHandler;
+import view.ConsoleView;
+
 public class KnowledgeController {
+    private KnowledgeController repository;
+    private ConsoleView view;
+    private InputHandler input;
+
     public KnowledgeController() {
+        repository = new KnowledgeController();
+        view = new ConsoleView();
+        input = new InputHandler();
 
     }
 }
