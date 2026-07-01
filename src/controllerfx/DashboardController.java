e00944035a7cf4f2a7c039682fb175624265d9c9
Adinda Rahmalia Jelita <adindarahmalia10@gmail.com>
2026-07-01 14:33:50 +0700

    feat: integrate dashboard javafx with model statistics

--- src/controllerfx/DashboardController.java
+++ src/controllerfx/DashboardController.java
@@ -1,4 +1,36 @@
 package controllerfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import model.KnowledgeRepository;
+import model.PutusanCsvLoader;
+import model.StatistikPutusan;
+
 public class DashboardController {
+    @FXML
+    private Label lblTotalPutusan;
+
+    @FXML
+    private Label lblRataVonis;
+
+    @FXML
+    private Label lblJenisTerbanyak;
+
+    private KnowledgeRepository repository;
+
+    @FXML
+    public void initialize() {
+        repository = new KnowledgeRepository();
+
+        PutusanCsvLoader.loadFromCsv(
+                "data/putusan_narkotika.csv",
+                repository
+        );
+        var data = repository.getDaftarSemua();
+        lblTotalPutusan.setText(String.valueOf(StatistikPutusan.totalPutusan(data)));
+        lblRataVonis.setText(String.format("%.2f Bulan", StatistikPutusan.rataRataVonis(data)));
+        lblJenisTerbanyak.setText(StatistikPutusan.jenisNarkotikaTerbanyak(data));
+
+    }
 }
