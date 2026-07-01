bc332e37274d4ed51d2ab3bcfe4d6d5a6d9c44a4
Adinda Rahmalia Jelita <adindarahmalia10@gmail.com>
2026-07-01 18:10:43 +0700

    feat: display most frequent narcotics type in statistics

--- src/controllerfx/StatistikController.java
+++ src/controllerfx/StatistikController.java
@@ -1,11 +1,29 @@
 package controllerfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import model.KnowledgeRepository;
+import model.StatistikPutusan;
 
 public class StatistikController {
 
     @FXML
+    private Label lblJenisTerbanyak;
+
+    private KnowledgeRepository repository;
+
+    @FXML
     public void initialize() {
 
+        repository = SharedRepository.getRepository();
+
+        var data = repository.getDaftarSemua();
+
+        lblJenisTerbanyak.setText(
+                StatistikPutusan.jenisNarkotikaTerbanyak(data)
+        );
+
     }
+
 }
