506354bb13186b964461cc09832a8479d55c0cb5
Yudissssssssssssssss <yudistira191206@gmail.com>
2026-07-02 17:00:26 +0700

    fix: remove controller dependency from model branch

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,14 +8,12 @@ import model.StatistikPutusan;
 public class Main {
 
     public static void main(String[] args) {
-
         KnowledgeRepository repo = new KnowledgeRepository();
 
         PutusanCsvLoader.loadFromCsv("data/putusan_narkotika.csv", repo);
 
         System.out.println("=== TOTAL DATA ===");
         System.out.println(repo.getTotalData());
-
 
         System.out.println("\n=== DATA PUTUSAN ===");
         for (Putusan p : repo.getDaftarSemua()) {
@@ -25,7 +23,7 @@ public class Main {
         System.out.println("\n=== STATISTIK ===");
         StatistikPutusan statUtama =
                 new StatistikPutusan(repo.getDaftarSemua());
-        statUtama.tampilkanLaporan();
 
+        statUtama.tampilkanLaporan();
     }
 }
